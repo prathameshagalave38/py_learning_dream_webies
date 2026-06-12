676b957e45fe2b13c457fa53beb4461fda0ad852
prathameshagalave38 <prathameshagalave38@gmail.com>
2026-06-12 14:46:57 +0530

    12 jun commited

---
+++ day12_12jun.py
@@ -0,0 +1,25 @@
+# disc practice
+
+
+
+Dictionary = {
+    "name":"prathamesh",
+    "age":"19",
+    "colleg":"gramin",
+    "dept":"co"
+}
+
+print(Dictionary)
+
+Dictionary["name"]="sandesh"
+print(Dictionary)
+
+
+
+colleg = {
+  "name": "gramin",
+  "dept": "co"
+ 
+}
+colleg.pop("name")
+print(colleg)
