c05b9f4a74360a06f4558eb5adb32b71471857e0
prathameshagalave38 <prathameshagalave38@gmail.com>
2026-06-12 15:11:38 +0530

    12 jun commited

--- task4_sun7may.py
+++ task4_sun7may.py
@@ -16,59 +16,59 @@ else:
 
 
 
-# Task 2: Magic Number
-secret = 7
+# # Task 2: Magic Number
+# secret = 7
 
-guess = int(input("Guess the secret number: "))
+# guess = int(input("Guess the secret number: "))
 
-if guess == secret:
-    print("🎉 You Win!")
-else:
-    print("❌ Try Again")
-
-
-
-
-#Task 3: Exam Result
-marks = int(input("Enter your marks: "))
-
-if marks >= 90:
-    print("A Grade")
-elif marks >= 75:
-    print("B Grade")
-elif marks >= 50:
-    print("C Grade")
-else:
-    print("Fail")
+# if guess == secret:
+#     print(" You Win!")
+# else:
+#     print(" Try Again")
 
 
 
 
-#Task 4: Superhero Entry
-cape = input("Do you have a cape? (yes/no): ")
+# #Task 3: Exam Result
+# marks = int(input("Enter your marks: "))
 
-if cape.lower() == "yes":
-    print("Welcome Superhero!")
-else:
-    print("You need a cape first.")
+# if marks >= 90:
+#     print("A Grade")
+# elif marks >= 75:
+#     print("B Grade")
+# elif marks >= 50:
+#     print("C Grade")
+# else:
+#     print("Fail")
 
 
+
+
+# #Task 4: Superhero Entry
+# cape = input("Do you have a cape? (yes/no): ")
+
+# if cape.lower() == "yes":
+#     print("Welcome Superhero!")
+# else:
+#     print("You need a cape first.")
 
 
 
 
 
-# Task 5: Rock Paper Scissors
-computer = "rock"
 
-user = input("Enter rock, paper or scissors: ")
 
-if user == computer:
-    print("Draw")
-elif user == "paper":
-    print("You Win")
-else:
-    print("Computer Wins")
+# # Task 5: Rock Paper Scissors
+# computer = "rock"
+
+# user = input("Enter rock, paper or scissors: ")
+
+# if user == computer:
+#     print("Draw")
+# elif user == "paper":
+#     print("You Win")
+# else:
+#     print("Computer Wins")
 
 
 
