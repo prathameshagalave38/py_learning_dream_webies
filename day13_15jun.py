5a6f61067a8376709ab72bf1d27950cd8e74f927
prathameshagalave38 <prathameshagalave38@gmail.com>
2026-06-15 16:01:36 +0530

    15 jun commited logic building day 1 dice problem

--- day13_15jun.py
+++ day13_15jun.py
@@ -47,3 +47,20 @@ if num == "6":
 
 
 # ans by geeks for geeks 
+
+def oppositeFaceOfDice(n):
+    if n == 1:
+        return 6
+    elif n == 2:
+        return 5
+    elif n == 3:
+        return 4
+    elif n == 4:
+        return 3
+    elif n == 5:
+        return 2
+    else:
+        return 1
+
+n = 2
+print(oppositeFaceOfDice(n))
