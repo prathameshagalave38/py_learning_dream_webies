b363c3075a2d0c5f9ab17731e6c407811701300c
prathameshagalave38 <prathameshagalave38@gmail.com>
2026-06-16 15:38:26 +0530

    16 jun commited looping

--- day14_16jun.py
+++ day14_16jun.py
@@ -3,3 +3,39 @@
 
 # SYNTAX :
     
+# for x in range(1,11):
+#     print(x)
+
+# print("  ")
+# for x in range(0,10):
+#     print(x)
+
+
+# print("  ")
+# for x in range(0,10):
+#     print("prathamesh")
+
+
+
+list1 =[9,5,3,8,1,8,9]
+
+for x in list1:
+    print(x)
+
+
+print("  ")
+list2 =[1,2,3,4,5,6,7,8,]
+
+for x in list2:
+    if x == 5:
+        break
+    print(x)
+
+
+
+
+print("  ")
+list2 =[1,2,3,4,5,6,7,8,]
+for x in list2:
+    if x == 5:
+    print(x)
