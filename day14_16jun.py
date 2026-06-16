8f7f8a3f96a7180d26c3a8a1058df792d803ebb6
prathameshagalave38 <prathameshagalave38@gmail.com>
2026-06-16 16:55:03 +0530

    16 jun commited looping for and while

--- day14_16jun.py
+++ day14_16jun.py
@@ -17,25 +17,110 @@
 
 
 
-list1 =[9,5,3,8,1,8,9]
+# list1 =[9,5,3,8,1,8,9]
 
-for x in list1:
-    print(x)
-
-
-print("  ")
-list2 =[1,2,3,4,5,6,7,8,]
-
-for x in list2:
-    if x == 5:
-        break
-    print(x)
+# for x in list1:
+#     print(x)
 
 
+# print("  ")
+# list2 =[1,2,3,4,5,6,7,8,]
+
+# for x in list2:
+#     if x == 5:
+#         break
+#     print(x)
 
 
-print("  ")
-list2 =[1,2,3,4,5,6,7,8,]
-for x in list2:
-    if x == 5:
-    print(x)
+
+
+# print("  ")
+# list2 =[1,2,3,4,5,6,7,8,]
+# for x in list2:
+#     print(x)
+
+
+
+
+# name =["prathamesh","sandesh","ram"]
+
+# for x in  name:
+#     if x == "prathameesh":
+#         print("present")
+#     else:
+#         print("absent")
+        
+
+
+
+
+# clg = ("gramn", "sahoyag", "matoshri")
+
+        
+
+
+
+# student = {
+#   "name": "prathamesh",
+#   "year": "second",
+#   "branch": "co"
+# }
+
+# for x in student.items():
+#     print(x, " ")
+
+
+# print("  ")
+# for x in student.values():
+#     print(x, " ")
+
+# print("  ")
+# for x in student.keys():
+#     print(x, " ")
+
+
+
+
+
+#list == 1 2 3 4 5 6 7 8
+
+# list1 = [1,2,3,4,5,6,7,8]
+
+# sum = 0
+# for x in list1 :
+#     sum = sum + x
+    
+
+# print(sum)
+
+
+
+
+
+
+
+
+#while loop
+
+# i = 1
+# while i <= 10:
+#   print(i)
+#   i += 1
+
+
+i = 0
+list1=["vip",2,"prathamesh",4,5,"raj",7,8,9]
+
+while i < len(list1):
+    print(list1[i], "  = ", i)
+    i = i + 1
+
+
+
+print("    ")
+i = 0
+list1=[1,2,3,4,5,6,7,8,9]
+
+while i < len(list1):
+    print(list1[i], "  = ", i)
+    i = i + 2
