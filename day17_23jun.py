92bbf044da2093ffa26ffc8cc88a215ba3a22b5a
prathameshagalave38 <prathameshagalave38@gmail.com>
2026-06-23 15:27:14 +0530

    23 jun polymorphusim and supper keyword commited

--- day17_23jun.py
+++ day17_23jun.py
@@ -23,3 +23,55 @@
 # obj1.card(12345)
 # obj2.card(12345)
 # obj3.card(12345)
+
+
+
+
+
+
+# class CLG :
+#     def __init__(self, name):
+#         self.name = name
+    
+
+# class RTN(CLG):
+#      def __init__(self, name ,age):
+#         super().__init__(name)
+#         self.age = age
+    
+    
+ 
+
+
+# obj3 =RTN("prathamesh" , 19)
+
+# print(obj3.name)
+# print(obj3.age)
+
+
+
+
+
+
+
+
+class CLG :
+    def __init__(self, name ,age):
+        self.name = name
+        self.age = age
+    
+
+class RTN(CLG):
+     def __init__(self, name ,age ,add):
+        super().__init__(name,age)
+        self.add = add
+    
+    
+ 
+
+
+obj3 =RTN("prathamesh" , 19 , "nanded")
+
+print(obj3.name)
+print(obj3.age)
+print(obj3.add)
