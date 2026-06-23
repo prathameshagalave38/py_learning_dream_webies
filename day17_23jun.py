64110797aa75e958cdb46267dca1237fc5856a60
prathameshagalave38 <prathameshagalave38@gmail.com>
2026-06-23 14:47:23 +0530

    13 jun polymorphusim commited

---
+++ day17_23jun.py
@@ -0,0 +1,25 @@
+# class ATM :
+#     def card(self,num):
+#        if num ==12345:
+#           print("done ATM")
+
+# class CLG :
+#     def card(self,num):
+#         if num == 12345 :
+#           print("done CLG")
+
+
+# class RTN:
+#     def card(self,num):
+#        if num == 12345 :
+#           print("done RTN")
+ 
+
+
+# obj1 = ATM()
+# obj2 =CLG()
+# obj3 =RTN()
+
+# obj1.card(12345)
+# obj2.card(12345)
+# obj3.card(12345)
