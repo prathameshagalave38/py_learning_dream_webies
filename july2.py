8b31e4a65b7d9a33ea133773d3047133dd1d2f55
prathameshagalave38 <prathameshagalave38@gmail.com>
2026-07-02 16:31:20 +0530

    july 2 commited server exception

--- july2.py
+++ july2.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+
 
 app = FastAPI()
+
 
 @app.get("/")
 def home():
@@ -10,8 +13,8 @@ def home():
 
 
 students ={
-    1:{"name":"prathamesh"},
-    2:{"name":"radheshyam"}
+    1:{"name":"prathamesh" , "age":"19"},
+    2:{"name":"radheshyam","age":"19"}
 }
 
 @app.get("/student/{stu_id}")
@@ -19,3 +22,17 @@ def show_studnt(stu_id : int):
     if stu_id not in students:
         raise HTTPException(status_code=404, detail="student not found")
     return students[stu_id]
+
+@app.get("/all")
+def show_all():
+    return students
+
+class students_details(BaseModel):
+    id : int
+    name : str
+    age : int
+
+@app.post("/details")
+def stud_data(stud_id : int , studentdata : stud_data ):
+    students[stud_id] = studentdata.dict()
+    return studentdata
